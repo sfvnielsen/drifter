de67809df955cebbbe4c9a76bbf41d3ee6d850bc
sfvnDTU <sfvn0410@gmail.com>
2015-01-08 12:02:09 +0100

    Added test *.txt files for likelihood calculation (obtained from SimpleTestData.m in code/test/matlab)

--- code/test/matlab/treeEdgeList.m
+++ code/test/matlab/treeEdgeList.m
@@ -1,4 +1,4 @@
-function [TEL,leaves] = treeEdgeList(T,I)
+function [TEL,LEL] = treeEdgeList(T,I)
 
 numberOfNodes = length(T);
 TEL = nan(numberOfNodes-1,2);
@@ -11,7 +11,9 @@ for i = 2:numberOfNodes
     end
 end
 
-leaves = sort(I-1);
+[leaves,dataID] = sort(I-1);
+
+LEL = [leaves',dataID'-1];
 
 %eof
 end
