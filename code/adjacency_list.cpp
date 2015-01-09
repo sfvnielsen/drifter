4d443621713771dcdcd0fd60bc9b954c68853c0f
JesperLH <Jesper.l@hinrich.dk>
2015-01-09 10:35:30 +0100

    Small changes to barebones_tree, and something with adj list (not done)

---
+++ code/adjacency_list.cpp
@@ -0,0 +1,10 @@
+//
+//  adjacency_list.cpp
+//  gift
+//
+//  Created by Jesper Hinrich on 09/01/15.
+//  Copyright (c) 2015 Jesper Hinrich. All rights reserved.
+//
+
+#include "adjacency_list.h"
+
