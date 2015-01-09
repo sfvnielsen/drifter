fe7837f0aa7d905f5ab2e71c7e6b25cdbd4beca3
JesperLH <Jesper.l@hinrich.dk>
2015-01-09 12:15:28 +0100

    Adjacency list before initial test

--- code/adjacency_list.h
+++ code/adjacency_list.h
@@ -11,85 +11,29 @@
 
 #include <stdio.h>
 
-#include <fstream>
-#include <iostream>
-#include <iomanip> //Setpresision
 #include <string>
 #include <vector>
-#include <math.h>
-#include <sys/time.h>	//Time taking Mac OS X
+//#include <math.h>
+#include <list>
+#include <algorithm> //std::binary_search, std::sort
+class Adj_list {
 
-#include "dotRead.h"
-#include "dijkstras.h"
-#include <ilcplex/ilocplex.h>
-
-
-class adjacency_list {
-    typedef int vertex_t;
-    typedef double weight_t;
-    
 public:
-    <#member functions#>
+    struct neighbor;
+    std::vector<std::vector<neighbor> > adjacency_list;
+    
+    Adj_list();
+    Adj_list(int,std::list<std::tuple<int,int>>);
+    int addDirectedEdge(int, int);
+    int addUndirectedEdge(int, int);
+    int removeDirectedEdge(int, int);
+    int removeUndirectedEdge(int, int);
+    bool existsConnection(int, int);
+    
     
 private:
-    
+
 };
-
-
-using namespace std;
-
-struct neighbor {
-    vertex_t target;
-    weight_t weight;
-    neighbor(vertex_t arg_target, weight_t arg_weight)
-    : target(arg_target), weight(arg_weight) { }
-};
-
-typedef std::vector<std::vector<neighbor> > adjacency_list_t;
-
-/*
- * Construct Adjacency list for G
- */
-adjacency_list_t constructAdjList(const vector<double >& G,
-                                  const vector<double>& duals,
-                                  const int & N,
-                                  const bool& C,
-                                  const int & numCommodities){
-    adjacency_list_t adjacency_list(N);
-    int count = 0;
-    if (C){
-        for (int i = 0; i < N; i++){
-            for (int j = 0; j < N; j++){
-                if (G[i*N+j] >= 0){
-                    adjacency_list[i].push_back(neighbor(j,G[i*N+j]));
-                    count++;
-                }
-            }
-        }
-    } else{
-        //Should be changed so it only goes though the arc which exist
-        // that information is available after the first iteration.
-        int nextInLine = 0;
-        cout << "Graph: Which dual range is selected?" << endl;
-        
-        for (int i = 0; i < N; i++){
-            for (int j = 0; j < N; j++){
-                if (G[i*N+j] >= 0){
-                    if (G[i*N+j]-duals[C+nextInLine] < 0){
-                        cout << "************ NEGATIVE VALUE IN ADJ LIST" <<
-                        "************" << endl;
-                        //Make the program quit!
-                    } else {
-                        adjacency_list[i].push_back(neighbor(j,G[i*N+j]-duals[numCommodities+nextInLine]));
-                    }
-                    nextInLine++;
-                    
-                }
-            }
-        }
-    }
-    return adjacency_list;
-}
 
 
 #endif /* defined(__gift__adjacency_list__) */
