e7f03a4c6480310e29bd28f42cbb7cf15847a15b
sfvnDTU <sfvn0410@gmail.com>
2015-01-13 15:58:26 +0100

    Fixed error in test files

--- code/test/matlab/treeEdgeList.m
+++ code/test/matlab/treeEdgeList.m
@@ -2,12 +2,13 @@ function [TEL,LEL] = treeEdgeList(T,I)
 
 numberOfNodes = length(T);
 TEL = nan(numberOfNodes-1,2);
+nodeID = 0:numberOfNodes;
 
 for i = 2:numberOfNodes
-    if T(T(i)) == 0
-        TEL(i-1,:) = [0,i-1];
+    if nodeID(T(i)) == 0
+        TEL(i-1,:) = [0,nodeID(i)];
     else
-        TEL(i-1,:)= [T(T(i)),i-1];
+        TEL(i-1,:)= [nodeID(T(i)),i-1];
     end
 end
 
