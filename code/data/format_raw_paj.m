ea365478241e392977fad84bc7f108f272927cc6
sfvnDTU <sfvn0410@gmail.com>
2015-01-15 16:57:41 +0100

    Removed some unessecary files from data folder and added new networks to test on

--- code/data/format_raw_paj.m
+++
@@ -1,8 +0,0 @@
-
-input_file = 'karate/karate.paj';
-output_file = 'karate_edgelist.txt';
-EL = undirected_paj2edgeL(input_file);
-
-f =  fopen(output_file,'w');
-fprintf(f,'%d %d\n',(EL-1)'); % -1 because MATLAB doesnt index at zero :S
-fclose(f);
