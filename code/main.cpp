17905c08848277cf86740e199aec507854e28b9b
sfvnDTU <sfvn0410@gmail.com>
2015-01-16 11:11:33 +0100

    IPS line added to main

--- code/main.cpp
+++ code/main.cpp
@@ -67,9 +67,8 @@ void testNetwork(string data_file_name, int num_of_iterations){
 
     std::cout << "finished computation at " << std::ctime(&end_time)
     << "elapsed time: " << elapsed_seconds.count() << " sec.\n"
-    << "mean elapsed time per regraft: " << elapsed_seconds.count()/((double) num_of_iterations) << " sec." << endl << endl;
-
-    cout << sampler.getLast().toString() << endl;
+    << "mean elapsed time per regraft: " << elapsed_seconds.count()/((double) num_of_iterations) << " sec." << endl << endl
+    << "Iterations per second: " << ((double) num_of_iterations)/(double)elapsed_seconds.count() << " ips" << endl;
 }
 
 Tree debuggingTree(){
