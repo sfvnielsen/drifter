3090ec68d80f40a9424895a279aa140b50b3b909
juliankopkalarsen <juliankopkalarsen@gmail.com>
2015-01-23 15:19:27 +0100

    main comments

--- code/main.cpp
+++ code/main.cpp
@@ -6,11 +6,9 @@
 */
 
 #include <iostream>
-#include <cstdlib> // setting random seed
 #include <time.h> // ----
 #include <chrono> // chrono::timepoint, chrono::system_clock
 
-#include "tree.h"
 #include "sampler.h"
 #include "iofilehandler.h"
 #include <random>
@@ -20,10 +18,6 @@ using namespace std;
 void testNetwork(string,int,int,int);
 
 int main() {
-    srand ((unsigned int) time(NULL)); // set random seed
-
-    std::random_device rd;
-    std::mt19937 random_generator(rd());
 
     string data_file_name = "data/karate_edgelist.txt";
     //data_file_name = "data/celegans_edgelist.txt";
@@ -34,9 +28,9 @@ int main() {
     /**
      * Testing a network
      */
-    int num_iterations = 100000;
-    int burnin = 100;
-    int thinning = 100;
+    int num_iterations = 1000;
+    int burnin = 1000;
+    int thinning = 10;
 
     testNetwork(data_file_name,num_iterations,burnin,thinning);
 
@@ -46,22 +40,29 @@ int main() {
  * Test a network specifed by an edge list when performing num_of_iterations
  */
 void testNetwork(string data_file_name, int num_of_iterations, int burnin, int thinning){
+    // Loading the data.
     cout << "Running on: " << data_file_name << endl;
     IoFileHandler data_file(data_file_name,0);
 
+    // Constructing the sampler.
     Sampler sampler = Sampler(data_file.getDataEl(), 0.5, 0.5, 1, 1);
 
+    // Start timing
     chrono::time_point<chrono::system_clock> start, end;
     start = chrono::system_clock::now();
 
+    // Run the sampler
     sampler.run(num_of_iterations, burnin, thinning);
 
+    // End timing
     end = chrono::system_clock::now();
     chrono::duration<double> elapsed_seconds = end-start;
     std::time_t end_time = std::chrono::system_clock::to_time_t(end);
 
+    // Print the last tree.
     //cout << sampler.getLast().toString() << endl;
 
+    // Print out statistics for the run.
     std::cout << "finished computation at " << std::ctime(&end_time)
     << "elapsed time: " << elapsed_seconds.count() << " sec.\n"
     << "mean elapsed time per regraft: " << elapsed_seconds.count()/((double) num_of_iterations) << " sec." << endl << endl
