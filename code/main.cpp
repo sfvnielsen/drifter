070b094728da009e1bdd04679edfef291e61eae5
juliankopkalarsen <juliankopkalarsen@gmail.com>
2015-04-11 17:54:03 +0200

    added accuracy printout to release

--- code/main.cpp
+++ code/main.cpp
@@ -22,7 +22,7 @@ void testNetwork(string,int,int,int);
 int main() {
 
     string data_file_name = "data/karate_edgelist.txt";
-    //data_file_name = "data/celegans_edgelist.txt";
+    data_file_name = "data/celegans_edgelist.txt";
     //data_file_name = "data/football_edgelist.txt";
     //data_file_name = "data/facebook100_edgelist.txt";
     //data_file_name = "data/uspower_edgelist.txt";
